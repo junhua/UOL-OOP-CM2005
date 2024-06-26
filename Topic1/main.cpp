129743a8d16f73bc9a2ee590ef401e9137932403
junhua <junhua.viva@gmail.com>
2024-06-26 23:04:55 +0800

    updated

--- Topic1/main.cpp
+++ Topic1/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 
-void print_hello();
+void print_plot();
+void print_lineplot();
+void print_plot(){
 
-void print_hello(int someNum){
-    std::cout << "Hello World!! Input: " << someNum << std::endl;
+std::cout << "10 |                                                   " << std::endl;
+std::cout << " 9 |        |                                 |        " << std::endl;
+std::cout << " 8 |        |                                ---       " << std::endl;
+std::cout << " 7 |       +++                               ---       " << std::endl;
+std::cout << " 6 |       +++               |               ---       " << std::endl;
+std::cout << " 5 |       +++               |                |        " << std::endl;
+std::cout << " 4 |        |               ---               |        " << std::endl;
+std::cout << " 3 |        |               ---               |        " << std::endl;
+std::cout << " 2 |        |                |                |        " << std::endl;
+std::cout << " 1 |                         |                         " << std::endl;
+std::cout << "-------------------------------------------------------" << std::endl;
+std::cout << "        1980/01           1980/02          1980/03     " << std::endl;
+}
+
+void print_lineplot(){
+
+std::cout << "10 |                                                   " << std::endl;
+std::cout << " 9 |                                                   " << std::endl;
+std::cout << " 8 |                                                   " << std::endl;
+std::cout << " 7 |                                                   " << std::endl;
+std::cout << " 6 |          --              --                       " << std::endl;
+std::cout << " 5 |        --  --          --                         " << std::endl;
+std::cout << " 4 |      --      --      --                           " << std::endl;
+std::cout << " 3 |                --  --                             " << std::endl;
+std::cout << " 2 |                  --                               " << std::endl;
+std::cout << " 1 |                                                   " << std::endl;
+std::cout << "-------------------------------------------------------" << std::endl;
+std::cout << "        1980/01           1980/02          1980/03     " << std::endl;
 }
 
 int main(){
-    print_hello(123);
+    print_plot();
     return 0;
 }
